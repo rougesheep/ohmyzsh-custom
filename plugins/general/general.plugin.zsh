6651c0a0aabc8c14d1f4455bee83a8f599b8735b
Andy Burne <andrewburne@rentalcars.com>
2016-05-06 09:36:02 +0100

    add some extra aliases

--- plugins/general/general.plugin.zsh
+++ plugins/general/general.plugin.zsh
@@ -4,3 +4,9 @@ alias editconf="vim ~/.zshrc"
 alias desktop-file-local-install="desktop-file-install --dir=$HOME/.local/share/applications/"
 alias tmux="tmux a"
 alias nsupdate="rlwrap nsupdate"
+alias gitup="git fetch && git pull"
+alias quit='exit'
+alias q='quit'
+alias nsupdate='rlwrap nsupdate'
+alias rssh='ssh -l root'
+alias sdig='dig +noall +answer'
